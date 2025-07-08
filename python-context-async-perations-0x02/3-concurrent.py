854f175665d4bc26b53c674f4e633590c4ab8a0c
Theresa Seyram Agbenyegah <seyramtheresa@gmail.com>
2025-07-08 14:14:06 +0000

    Update the Repository with Python Context async operations

--- python-context-async-perations-0x02/3-concurrent.py
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -13,7 +13,7 @@ async def async_fetch_users():
                 print(user)
 
 
-async def async_fetch_older_users():
+async def async_fetch_holder_users():
     async with aiosqlite.connect(DB_NAME) as db:
         async with db.execute("SELECT * FROM users WHERE age > 40") as cursor:
             older_users = await cursor.fetchall()
@@ -23,7 +23,7 @@ async def async_fetch_older_users():
 
 
 async def fetch_concurrently():
-    await asyncio.gather(async_fetch_users(), async_fetch_older_users())
+    await asyncio.gather(async_fetch_users(), async_fetch_holder_users())
 
 
 if __name__ == "__main__":
